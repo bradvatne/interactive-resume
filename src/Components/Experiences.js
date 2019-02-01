8c871e5cca390a3bb2a5faa3449693176d45b83d
notbrad <45443057+notbrad@users.noreply.github.com>
2019-01-31 19:51:45 -0800

    Small edits

--- src/Components/Experiences.js
+++ src/Components/Experiences.js
@@ -5,7 +5,7 @@ export class Experiences extends Component {
     return (
       <div className="container">
           <h1>Experiences</h1><br/><p>LEARN, EARN, EXPLORE, REPEAT</p><br/><hr/><br/>
-
+          <div className="overflow">
           <h3 className="left">EDUCATION</h3><br/>
                 <div className="experiences-box">
                     <div className="experiences-title"><p><strong>UNIVERSITY OF VICTORIA</strong><br/><br/>Sept 2009 - April 2011,<br/>May 2018 - Present</p></div>
@@ -16,6 +16,16 @@ export class Experiences extends Component {
                 <div className="experiences-box">
                     <div className="experiences-title"><p><strong>MOVEMENT FOOD</strong><br/><br/>June 2017 - Sept 2018</p></div>
                     <div className="experiences-description"><p><strong>BACHELOR OF SCIENCE - COMPUTER SCIENCE AND PSYCHOLOGY (COMBINED)</strong><br/><br/>I studied general sciences in my first two years, including classes such as; calculus, physics, organic chemistry, biology and psychology. Feeling a lack of direction, I took a few years off before returning enthusiastically to pursue a combined major of Computer Science and Psychology. <br/>The GPA of courses taken since my return is currently 3.8 .</p></div>
+                </div>
+          <div className="experiences-box">
+              <div className="experiences-title"><p><strong>MOVEMENT FOOD</strong><br/><br/>June 2017 - Sept 2018</p></div>
+              <div className="experiences-description"><p><strong>BACHELOR OF SCIENCE - COMPUTER SCIENCE AND PSYCHOLOGY (COMBINED)</strong><br/><br/>I studied general sciences in my first two years, including classes such as; calculus, physics, organic chemistry, biology and psychology. Feeling a lack of direction, I took a few years off before returning enthusiastically to pursue a combined major of Computer Science and Psychology. <br/>The GPA of courses taken since my return is currently 3.8 .</p></div>
+          </div>
+
+          <div className="experiences-box">
+              <div className="experiences-title"><p><strong>MOVEMENT FOOD</strong><br/><br/>June 2017 - Sept 2018</p></div>
+              <div className="experiences-description"><p><strong>BACHELOR OF SCIENCE - COMPUTER SCIENCE AND PSYCHOLOGY (COMBINED)</strong><br/><br/>I studied general sciences in my first two years, including classes such as; calculus, physics, organic chemistry, biology and psychology. Feeling a lack of direction, I took a few years off before returning enthusiastically to pursue a combined major of Computer Science and Psychology. <br/>The GPA of courses taken since my return is currently 3.8 .</p></div>
+          </div>
                 </div>
                 </div>
     )
