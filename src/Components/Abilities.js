da09554e3d6bae9d99fe8e14d9ffe64e70ea0b93
notbrad <45443057+notbrad@users.noreply.github.com>
2019-02-05 15:17:58 -0800

    Added Abilities

--- src/Components/Abilities.js
+++ src/Components/Abilities.js
@@ -2,11 +2,77 @@ import React, { Component } from "react";
 
 export class Abilities extends Component {
   render() {
+
     return (
+
       <div className="fadeIn">
+      
         <hr />
-        <h2>//TODO</h2>
-        <hr />
+        <h2>ABILITIES</h2>
+        <hr/>
+        <div className="abilities-container">
+        <table>
+        <tr>
+          <th><h3>LANGUAGE</h3></th>
+          <th><h3>LEVEL</h3></th>
+        </tr>
+        <tr>
+        <td>HTML</td>
+        <td><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i></td>
+        </tr>
+        <tr>
+        <td>CSS</td>
+        <td><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i></td>
+        </tr>
+        <tr>
+        <td>Javascript</td>
+        <td><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i></td>
+        </tr>
+        <tr>
+        <td>React.js</td>
+        <td><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star-half" aria-hidden="true"></i></td>
+        </tr>
+        <tr>
+        <td>Python</td>
+        <td><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star-half" aria-hidden="true"></i></td>
+        </tr>
+        <tr>
+        <td>Java</td>
+        <td><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star-half" aria-hidden="true"></i></td>
+        </tr>
+        </table>
+        <hr/>
+        <table>
+        <tr>
+          <th><h3>SKILLS</h3></th>
+          <th><h3>LEVEL</h3></th>
+        </tr>
+        <tr>
+        <td>Github + Git</td>
+        <td><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i></td>
+        </tr>
+        <tr>
+        <td>Problem Solving</td>
+        <td><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i></td>
+        </tr>
+        <tr>
+        <td>Communication</td>
+        <td><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i></td>
+        </tr>
+        <tr>
+        <td>Resourcefulness</td>
+        <td><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i></td>
+        </tr>
+        <tr>
+        <td>Sociability</td>
+        <td><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i></td>
+        </tr>
+        <tr>
+        <td>UI Design</td>
+        <td><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i></td>
+        </tr>
+        </table>
+      </div>
       </div>
     );
   }
