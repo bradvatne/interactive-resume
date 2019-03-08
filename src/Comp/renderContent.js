e545055563c437485b46e41587e67c74efff7734
notbrad <45443057+notbrad@users.noreply.github.com>
2019-03-08 11:20:43 -0800

    Working on refactoring with es6 functions

--- src/Comp/renderContent.js
+++ src/Comp/renderContent.js
@@ -1,4 +1,5 @@
 import React, {Fragment} from "react";
+import content from "../content";
 
 //Refactored for modularity mar 7 2019
 
@@ -7,7 +8,7 @@ export const renderTitle = props => {
   return (
     <div className="experiences-title">
       <h3>{props.name}</h3>
-      {props.url != undefined ? (
+      {props.url !== undefined ? (
         <p>{certificate(props.url)}</p>
       ) : (
         <p>{props.date}</p>
@@ -48,4 +49,91 @@ export const renderContentBox = props => {
       <hr />
     </Fragment>
   );
+};
+
+export const renderLanguageTable = (props) => {
+  
+  // const langs = Object.keys(props)
+  const levels = Object.values(props)
+  // console.log(langs)
+  console.log(levels)
+
+  levels.map()
+
+
+  return (
+    <div className="abilities-container">
+      <table>
+        <tbody>
+          <tr>
+            <th>
+              <h3>LANGUAGE</h3>
+            </th>
+            <th>
+              <h3>LEVEL</h3>
+            </th>
+          </tr>
+        </tbody>
+      </table>
+      <hr />
+    </div>
+  );
+};
+
+export const renderToolsTable = props => {
+  return (
+  <table>
+  <tbody>
+    <tr>
+      <th>
+        <h3>TOOLS/CONCEPTS</h3>
+      </th>
+      <th> </th>
+    </tr>
+    <tr>
+      <td>Github/Git</td>
+      <td>NPM</td>
+    </tr>
+    <tr>
+      <td>Axios</td>
+      <td>AJAX</td>
+    </tr>
+    <tr>
+      <td>SQLlite</td>
+      <td>Django</td>
+    </tr>
+    <tr>
+      <td>Redux</td>
+      <td>React-Router</td>
+    </tr>
+    <tr>
+      <td>Component Lifecycle</td>
+      <td>Flexbox/Grid</td>
+    </tr>
+    <tr>
+      <td>Functional Programs</td>
+      <td>OOP's</td>
+    </tr>
+  </tbody>
+</table>
+  )
+}
+
+
+// export const renderLanguage = props => {
+
+//   return (
+//     <tr>
+//       <td>{props.name}</td>
+//       <td>
+//         {stars.map(() => (
+//           <i className="fa fa-star" aria-hidden="true" />
+//         ))}
+//       </td>
+//     </tr>
+//   );
+// };
+
+export const test = () => {
+  for (let lang in content.knowledge.languages) console.log(lang.name);
 };
--- src/Containers/Knowledge.js
+++ src/Containers/Knowledge.js
@@ -1,93 +1,16 @@
 import React from "react";
 import content from "../content";
+import {test, renderLanguageTable, renderToolsTable} from "../Comp/renderContent"
+
 
 const Knowledge = () => {
 
-  const { languages } = content.knowledge;
-  const stars = [];
-  const languagesArray = [];
-
-  const renderLanguage = props => {
-
-    for (let i = 0; i < props.level; i++) {
-      stars.push(i);
-    }
-
-    for (let language in languages) {
-      languagesArray.push(language.name);
-    }
-
-    return (
-      <tr>
-        <td>{props.name}</td>
-        <td>
-          {stars.map(() => (
-            <i className="fa fa-star" aria-hidden="true" />
-          ))}
-        </td>
-      </tr>
-    );
-  };
-
-  console.log(languages)
-  console.log(languagesArray)
-  const langs = languagesArray.map(language => renderLanguage(language))
-
-  
+  test()
   return (
     <div className="fadeIn">
-      <hr />
-      <div className="abilities-container">
-        <table>
-          <tbody>
-            <tr>
-              <th>
-                <h3>LANGUAGE</h3>
-              </th>
-              <th>
-                <h3>LEVEL</h3>
-              </th>
-            </tr>
-            {langs}
-          </tbody>
-        </table>
-        <hr />
-        <table>
-          <tbody>
-            <tr>
-              <th>
-                <h3>TOOLS/CONCEPTS</h3>
-              </th>
-              <th> </th>
-            </tr>
-            <tr>
-              <td>Github/Git</td>
-              <td>NPM</td>
-            </tr>
-            <tr>
-              <td>Axios</td>
-              <td>AJAX</td>
-            </tr>
-            <tr>
-              <td>SQLlite</td>
-              <td>Django</td>
-            </tr>
-            <tr>
-              <td>Redux</td>
-              <td>React-Router</td>
-            </tr>
-            <tr>
-              <td>Component Lifecycle</td>
-              <td>Flexbox/Grid</td>
-            </tr>
-            <tr>
-              <td>Functional Programs</td>
-              <td>OOP's</td>
-            </tr>
-          </tbody>
-        </table>
+      {renderLanguageTable(content.knowledge.languages)}
+      {renderToolsTable()}
       </div>
-    </div>
   );
 };
 
