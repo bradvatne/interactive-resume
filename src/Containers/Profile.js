a97d0ac1bf9a9bcfbaf60323126968971a94a2e1
notbrad <45443057+notbrad@users.noreply.github.com>
2019-03-10 14:07:23 -0700

    Update Profile.js

--- src/Containers/Profile.js
+++ src/Containers/Profile.js
@@ -9,8 +9,9 @@ const Profile = () => {
       <div className="aboutme-boxes">
         <div className="aboutme-box1">
           <br />
-          {content.aboutme.replace(/\n/g,"<br/>")}
-          <p />
+          <p>
+          Brad Vatne is a full-time student at the University of Victoria, graduating in Fall of 2020 with a Bachelor of Science in Computer Science and Psychology as a combined major.<br/><br/> In his spare time, he enjoys trying out new restaurants with his girlfriend, reading, running, hiking, practicing yoga, listening to podcasts, and writing code of all sorts. <br/><br/>Throughout his life, he has carried a strong passion for creative expression. He finds that software development combines an opportunity to release his creative energy while simultaneously challenging himself intellectually. Naturally, he has fallen in love with it. <br/><br/>He has chosen Javascript as his main language because of its versatility and wide range of libraries, especially React. In the future, he has plans to learn cross-platform libraries like React-Native and Electron.
+          </p>
         </div>
         <div className="aboutme-box2">
           <img src={Portrait} alt="Brad's Portrait" />
